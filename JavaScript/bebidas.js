1ac8e332b7888ce78de86bf4c3918e7f3ccdb2c4
kdom99 <kdom99@educa.madrid.org>
2024-02-18 19:37:17 +0100

    18/17

--- JavaScript/bebidas.js
+++ JavaScript/bebidas.js
@@ -10,8 +10,15 @@ document.addEventListener("keyup", e=>{
               ?cafe.classList.remove("filtro")
               :cafe.classList.add("filtro")
         })
-  
     }
-  
-  
   })
+
+  function ej6() {
+    swal({
+      
+    })
+    var precio = prompt("Indica el precio del producto: ");
+    var cantidad = prompt("Indica la cantidad a comprar: ");
+
+    alert("El cliente se quiere llevar " + cantidad + " artículos, que cuestan " + precio + " cada uno. El total de su compra es: " + parseFloat(precio) * parseInt(cantidad));
+}
